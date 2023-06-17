7f8f158a37efb6b373ca5658459d8e92038da98f
Yanai1005 <tatsuya871_1005@icloud.com>
2023-06-17 20:19:27 +0900

    [edit]overview

--- app/playTimer/page.js
+++ app/playTimer/page.js
@@ -13,7 +13,7 @@ const PlayTimer = () => {
 
   const handleEnterKey = (event) => {
     if (event.key === 'Enter') {
-      setHypotheses((prevHypotheses) => [inputValue, ...prevHypotheses]);
+      setHypotheses((prevHypotheses) => [inputValue, ...prevHypotheses.slice(0, 9)]);
       setInputValue('');
     }
   };
@@ -31,6 +31,7 @@ const PlayTimer = () => {
           <p>- 分からないところが分からない</p>
           <p>- 分からないところが分からない状況を何とかする術を知らない</p>
           <h2>悩んでいること</h2>
+          <input></input>
           <h2>悩みから得られる仮説</h2>
           <input
             type="text"
@@ -43,7 +44,8 @@ const PlayTimer = () => {
               <p key={index}>{hypothesis}</p>
             ))}
           </div>
-            <h2>仮説から得られた結論</h2>
+          <h2>仮説から得られた結論</h2>
+          <input></input>
         </div>
       </div>
     </main>
