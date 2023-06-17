5d1d39b925f97d95f22be789f9fb7f33085d19a6
kamakura0523 <suieikama0523@gmail.com>
2023-06-17 15:47:20 +0900

    user_idとpassword作成

--- frontend/pomodoro/components/Button.js
+++ frontend/pomodoro/components/Button.js
@@ -2,12 +2,20 @@ import React from 'react';
 const Button = () => {
   return (
     <div className="login button">
-            {/* <Link href="/login/"> */}
-        <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            ログイン
-        </button>
+      <h3>ログイン</h3>
+      <p>
+        <input type="id" className="bg-[#E0DCD1]"  placeholder="user name" margin ></input> </p>
+      
+        
+        <input type="id" className="bg-[#E0DCD1]"  placeholder="password"></input> 
+        
+          {/* <Link href="/login/"> */}
+          <button  className="bg-[#75533C] hover:bg-[#AF9268] text-white font-bold py-2 px-4 rounded">
+              ログイン
+          </button>
+      
         {/* </Link> */}
-        </div>
+    </div>
   );
 }
 export default Button
