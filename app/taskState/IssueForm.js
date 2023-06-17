60516f167201484ef418e086d6e54b0a02499808
kamakura0523 <suieikama0523@gmail.com>
2023-06-17 22:12:04 +0900

    丸くした

--- app/taskState/IssueForm.js
+++ app/taskState/IssueForm.js
@@ -6,8 +6,8 @@ const IssueForm = () => {
     <div className="issue-form">
       <form>
         <div className="form-group">
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="タスク名" />
-          <button type="submit" className="btn btn-primary"></button>
+          <input type="text" className="form-control border-2 text-2xl  rounded-l-md " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="タスク名" />
+          <button type="submit" className="btn btn-primary bg-cyan-500 border-2 text-2xl rounded-r-md ">追加</button>
         </div>
       </form>
     </div>
