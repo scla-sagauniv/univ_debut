898392a034311cd3ef8c2bf9375a306ca214b899
Yanai1005 <tatsuya871_1005@icloud.com>
2023-06-17 20:36:26 +0900

    [add]start and stop button

--- components/Timer.js
+++ components/Timer.js
@@ -2,10 +2,12 @@
 import { useState, useEffect } from 'react';
 
 const Timer = () => {
-  const [time, setTime] = useState(900); // 初期値は900秒 (15分)
-  const [Run, setRun] = useState(false);
+  const [time, setTime] = useState(900);
+  const [run, setRun] = useState(false);
   useEffect(() => {
-    const countDown = setInterval(() => {
+    let countDown;
+    if(run){
+    countDown = setInterval(() => {
       setTime(Time => {
         if (Time === 0) {
           clearInterval(countDown);
@@ -15,19 +17,42 @@ const Timer = () => {
         return Time - 1;
       });
     }, 1000);
-
+  }
     return () => {
       clearInterval(countDown);
     };
-  }, []);
-
-  const minute = Math.floor(time / 60);
-  const second = time - minute * 60;
-
+  }, [run]);
+  
+  const Start = () => {
+    setRun(true);
+  };
+  
+  const Stop = () => {
+    setRun(false);
+  };
+const minute = Math.floor(time / 60);
+const second = time - minute * 60;
   return (
     <div>
-      <h2 style={{ fontSize: '2rem' }}> {minute}分 {second}秒</h2>
+    <h2 style={{ fontSize: '2rem' }}>
+      {minute}分 {second}秒
+    </h2>
+    <div className="flex justify-center mt-4">
+      {run ? (
+        <button
+          onClick={Stop}
+        >
+          ストップ
+        </button>
+      ) : (
+        <button
+          onClick={Start}
+        >
+          スタート
+        </button>
+      )}
     </div>
+  </div>
   );
 };
 
