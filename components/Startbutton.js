fa840ff8b63287d11455b1a168a265897166ea90
kamakura0523 <suieikama0523@gmail.com>
2023-06-18 15:15:32 +0900

    start画面

--- components/Startbutton.js
+++ components/Startbutton.js
@@ -5,7 +5,7 @@ const Startbutton = () => {
     return (
         <div className="flex justify-center">
             <Link href="/taskState/">
-        <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button  className="bg-[#75533C] hover:bg-[#AF9268] text-white font-bold py-10 px-20 rounded text-6xl">
             Start
         </button>
         </Link>
