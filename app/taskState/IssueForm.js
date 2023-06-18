795b2bb86bbfd8ea14419bf21667a3aec487c7bf
Yanai1005 <tatsuya871_1005@icloud.com>
2023-06-18 15:05:14 +0900

    [fix]miss

--- app/taskState/IssueForm.js
+++ app/taskState/IssueForm.js
@@ -19,7 +19,7 @@ const IssueForm = () => {
       <form onSubmit={Submit}>
         <div className="form-group">
           <input type="text" className="form-control border-2 border-current text-2x1  rounded-l-md px-4 py-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="タスク名"   value={title}
-          onChange={TitleChange />
+          onChange={TitleChange} />
           <button type="submit" className="btn btn-primary bg-cyan-200 border-2 border-current text-2x1 rounded-r-md  px-4 py-2">追加</button> 
         </div>
       </form>
